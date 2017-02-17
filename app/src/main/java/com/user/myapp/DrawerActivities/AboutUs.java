e52a0fa5d5011b54edade77a5866df67e3d0873c
Alex55795579@gmail.com <system5579>
2017-02-18 04:48:48 +0600

    Toggle

--- app/src/main/java/com/user/myapp/DrawerActivities/AboutUs.java
+++ app/src/main/java/com/user/myapp/DrawerActivities/AboutUs.java
@@ -1,15 +1,32 @@
 package com.user.myapp.DrawerActivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.user.myapp.MainActivity;
 import com.user.myapp.R;
 
 public class AboutUs extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        button = (Button) findViewById(R.id.btn_main);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutUs.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/user/myapp/DrawerActivities/News.java
+++ app/src/main/java/com/user/myapp/DrawerActivities/News.java
@@ -2,14 +2,20 @@ package com.user.myapp.DrawerActivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.user.myapp.R;
 
 public class News extends AppCompatActivity {
 
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        textView = (TextView) findViewById(R.id.tv_news);
+        textView.setText("fil.txt");
     }
 }
