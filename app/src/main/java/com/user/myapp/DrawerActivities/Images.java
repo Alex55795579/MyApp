afd731523e3b0a82f4aebe541c3a7b96dbe98208
Alex55795579@gmail.com <system5579>
2017-02-24 12:20:50 +0600

    ImageView, switch, Logs

--- app/src/main/java/com/user/myapp/DrawerActivities/Images.java
+++ app/src/main/java/com/user/myapp/DrawerActivities/Images.java
@@ -2,13 +2,20 @@ package com.user.myapp.DrawerActivities;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.user.myapp.MainActivity;
 import com.user.myapp.R;
 
 public class Images extends MainActivity {
+
+    Button btn, btn1, btn2;
+    ImageView imageView;
+    private static final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,37 @@ public class Images extends MainActivity {
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View contentView = inflater.inflate(R.layout.activity_images, null, false);
         drawerLayout.addView(contentView, 0);
+
+        Log.d(TAG, "find view");
+        btn = (Button) findViewById(R.id.im1_btn);
+        btn1 = (Button) findViewById(R.id.im2_btn);
+        btn2 = (Button) findViewById(R.id.im3_btn);
+        imageView = (ImageView) findViewById(R.id.im_v);
+
+        View.OnClickListener vol = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                switch (v.getId()){
+                    case R.id.im1_btn:
+                        imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_contact_mail_black_18dp));
+                        Log.d(TAG, "button 1 pressed");
+                        break;
+                    case R.id.im2_btn:
+                        imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_archive_black_18dp));
+                        Log.d(TAG, "button 2 pressed");
+                        break;
+                    case R.id.im3_btn:
+                        imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_magnify_black_18dp));
+                        Log.d(TAG, "button default pressed");
+                        break;
+
+                }
+            }
+        };
+        btn.setOnClickListener(vol);
+        btn1.setOnClickListener(vol);
+        btn2.setOnClickListener(vol);
 
     }
 
